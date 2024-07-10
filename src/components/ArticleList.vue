3ba5689a233bbea9765b624f8c661a1d9f8643b9
David Burke <474346+bettlebrox@users.noreply.github.com>
2024-07-10 09:02:26 +0100

    fix: formatting

--- src/components/ArticleList.vue
+++ src/components/ArticleList.vue
@@ -4,6 +4,7 @@
       <v-list-item v-for="article in articles" :key="article.id" :title="article.original_title" :href="article.url"
         link>
         <v-list-item-subtitle >{{ article.url }}</v-list-item-subtitle>
+        <v-list-item-subtitle>{{ formatDate(article.logged_at) }}</v-list-item-subtitle>
         <span class="font-weight-light">{{ article.summary }}</span>
           <v-chip v-for="theme in article.themes" :to="'theme/' + theme" link>
           {{ theme }}
@@ -23,6 +24,11 @@ export default {
       type: String,
       required: true
     }
+  },
+  methods:{
+    formatDate(date) {
+      return date.split('T')[0] + " " + date.split('T')[1].split('.')[0]
+    },
   }
 }
 </script>
